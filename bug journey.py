367829cdcff16222ef255697e1b7a7b3351fc7dd
gujiu-2KPJ <gujiu47@gmail.com>
2026-05-30 12:27:59 +0800

    update log fuction

--- bug journey.py
+++ bug journey.py
@@ -9,6 +9,9 @@ def save_error (error_message):
     with open('bug_log.txt','a',encoding= 'UTF-8') as f:
         f.write(f"[{time_str}]{error_message}\n")
 # bug_journey.py - Bug日志记录模块
+def view_log():
+    with open ("bug_log.txt","r",encoding='UTF-8') as f:
+        print(f.read())
 
 if __name__== '__main__':
     print(f'{record_time()}')
@@ -17,7 +20,7 @@ if __name__== '__main__':
         result = 10/0
     except Exception as e:
         save_error(str(e))#将错误变成字符串写入到buglog文件里面
-
+    view_log()
          
 
     
